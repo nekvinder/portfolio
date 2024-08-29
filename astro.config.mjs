5e5fd758ec8043d97827d6f6bad01f4b92920ebc
nekvinder <nekvinder@gmail.com>
2024-08-29 13:53:36 +0530

    remove server config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 import icon from "astro-icon";
 
@@ -10,8 +9,4 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://nekvinder.com",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
